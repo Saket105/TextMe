beabf30d969b1e46f7b5d0b41f0c82a41caeee6c
Saket Kumar <saketcool35@gmail.com>
2021-06-17 01:06:47 +0530

    Updating Project

--- app/src/main/java/com/example/textme/LoginActivity.java
+++ app/src/main/java/com/example/textme/LoginActivity.java
@@ -1,27 +1,31 @@
 package com.example.textme;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    TextInputLayout numberInputLayout;
+    Button continueButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +35,57 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
-
+        numberInputLayout = findViewById(R.id.number);
+        continueButton = findViewById(R.id.continue1);
+        progressBar = findViewById(R.id.progressBar);
         auth = FirebaseAuth.getInstance();
+
+        continueButton.setOnClickListener(view -> {
+            if (!numberInputLayout.getEditText().getText().toString().trim().isEmpty()) {
+                if (numberInputLayout.getEditText().getText().toString().trim().length() == 10) {
+//                    startActivity(new Intent(this,OtpVarificationActivity.class)
+//                            .putExtra("mobile",numberInputLayout.getEditText().getText().toString()));
+
+                    progressBar.setVisibility(View.VISIBLE);
+                    continueButton.setVisibility(View.INVISIBLE);
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            "+91" + numberInputLayout.getEditText().getText().toString(),
+                            60,
+                            TimeUnit.SECONDS,
+                            LoginActivity.this,
+                            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                @Override
+                                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                    progressBar.setVisibility(View.GONE);
+                                    continueButton.setVisibility(View.VISIBLE);
+                                }
+
+                                @Override
+                                public void onVerificationFailed(@NonNull FirebaseException e) {
+                                    progressBar.setVisibility(View.GONE);
+                                    continueButton.setVisibility(View.VISIBLE);
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+                                @Override
+                                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+//                                    super.onCodeSent(s, forceResendingToken);
+                                    progressBar.setVisibility(View.GONE);
+                                    continueButton.setVisibility(View.VISIBLE);
+                                    startActivity(new Intent(LoginActivity.this, OtpVarificationActivity.class)
+                                            .putExtra("mobile", numberInputLayout.getEditText().getText().toString())
+                                            .putExtra("backendOtp",s));
+                                }
+                            }
+                    );
+                } else {
+                    numberInputLayout.getEditText().setError("Invalid Number!");
+                    return;
+                }
+            } else {
+                Toast.makeText(this, "First enter mobile number", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/textme/OtpVarificationActivity.java
+++ app/src/main/java/com/example/textme/OtpVarificationActivity.java
@@ -1,19 +1,41 @@
 package com.example.textme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.concurrent.TimeUnit;
 
 public class OtpVarificationActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
     DatabaseReference reference;
-
-
+    EditText input1, input2, input3, input4, input5, input6;
+    TextView showNumber, againRequest;
+    Button signUp;
+    String getOtp, enterCodeOtp;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +45,222 @@ public class OtpVarificationActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+        input3 = findViewById(R.id.input3);
+        input4 = findViewById(R.id.input4);
+        input5 = findViewById(R.id.input5);
+        input6 = findViewById(R.id.input6);
+        showNumber = findViewById(R.id.showNumber);
+        signUp = findViewById(R.id.register);
+        againRequest = findViewById(R.id.again_request);
+        progressBar = findViewById(R.id.progressBar1);
         auth = FirebaseAuth.getInstance();
+
+        showNumber.setText(String.format("+91-%s", getIntent().getStringExtra("mobile")));
+
+        getOtp = getIntent().getStringExtra("backendOtp");
+
+        signUp.setOnClickListener(view -> {
+            if (!input1.getText().toString().trim().isEmpty() &&
+                    !input2.getText().toString().trim().isEmpty() &&
+                    !input3.getText().toString().trim().isEmpty() &&
+                    !input4.getText().toString().trim().isEmpty() &&
+                    !input5.getText().toString().trim().isEmpty() &&
+                    !input6.getText().toString().trim().isEmpty()) {
+                //Toast.makeText(this, "Otp Verify", Toast.LENGTH_SHORT).show();
+
+                 enterCodeOtp = input1.getText().toString() +
+                        input2.getText().toString() +
+                        input3.getText().toString() +
+                        input4.getText().toString() +
+                        input5.getText().toString() +
+                        input6.getText().toString();
+
+                if (getOtp != null) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    signUp.setVisibility(View.INVISIBLE);
+                    SigningUser();
+
+                } else {
+                    Toast.makeText(this, "Check Internet Connection!", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                Toast.makeText(this, "Please enter correct OTP!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        numberOtpMoved();
+        againRequest.setOnClickListener(view -> {
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                    "+91" + getIntent().getStringExtra("mobile"),
+                    60,
+                    TimeUnit.SECONDS,
+                    OtpVarificationActivity.this,
+                    new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                        @Override
+                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        }
+
+                        @Override
+                        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                            Toast.makeText(OtpVarificationActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCodeSent(@NonNull String s1, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+//                                    super.onCodeSent(s, forceResendingToken);
+                            getOtp = s1;
+                            Toast.makeText(getApplicationContext(),"Otp Send Successfully",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+        });
+    }
+
+    private void SigningUser() {
+        PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(getOtp, enterCodeOtp);
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                signUp.setVisibility(View.VISIBLE);
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(getApplicationContext(), ChatScreen.class));
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(OtpVarificationActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void numberOtpMoved() {
+
+        input1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    input2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        input2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    input3.requestFocus();
+                } else if (charSequence.toString().trim().isEmpty()) {
+                    input1.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        input3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    input4.requestFocus();
+                } else if (charSequence.toString().trim().isEmpty()) {
+                    input2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        input4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    input5.requestFocus();
+                } else if (charSequence.toString().trim().isEmpty()) {
+                    input3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        input5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()) {
+                    input6.requestFocus();
+                } else if (charSequence.toString().trim().isEmpty()) {
+                    input4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        input6.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().isEmpty()) {
+                    input5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
